fc3342ce72c27b48ad67daddfb19badb16155346
ManikantaMandala <mandala.manikanta20cse@bmu.edu.in>
2025-04-08 16:52:48 +0530

    refactor: refactored Doctor.java and Patient.java

--- src/main/java/com/hcltech/doctor_patient_service/model/Doctor.java
+++ src/main/java/com/hcltech/doctor_patient_service/model/Doctor.java
@@ -1,35 +1,44 @@
 package com.hcltech.doctor_patient_service.model;
 
-import java.util.UUID;
-
-import org.hibernate.annotations.Cascade;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Doctor {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @Column(name = "doctor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "doctor_name", nullable = false)
     private String name;
+
+    @Column(name = "doctor_specialization", nullable = false)
     private String specialization;
-    @OneToMany
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name="patient_id_f_key", referencedColumnName = "id")
     private List<Patient> patients;
+
     @OneToMany
+    @JoinColumn(name="appointment_id_f_key")
     private List<Appointment> appointments;
 }
--- src/main/java/com/hcltech/doctor_patient_service/model/Patient.java
+++ src/main/java/com/hcltech/doctor_patient_service/model/Patient.java
@@ -1,20 +1,39 @@
 package com.hcltech.doctor_patient_service.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-
 import java.util.UUID;
 
-@Entity
-public class Patient {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
     @Id
-    UUID id;
-    String name;
-    int age;
-   @OneToOne
-    @JoinColumn(name="d_id",referencedColumnName ="id")
-    Doctor doctor;
+    @Column(name = "patient_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "patient_name")
+    private String name;
+
+    @Column(name = "patient_age")
+    private int age;
+
+    @ManyToOne()
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
 }
